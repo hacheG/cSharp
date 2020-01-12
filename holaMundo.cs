b47338406849e17ecc7ab201bc35d094451462d8
hacheG <943@holbertonschool.com>
2020-01-12 11:58:38 -0500

    [helloWorld]

---
+++ holaMundo.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace holaMundo
+{
+    class holaMundo
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Microsoft Love Linux");
+        }
+    }
+}
